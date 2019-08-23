7aaafabaaa68ee1f8e8cd80d1bd586921ad87e86
Yevhen Shymotyuk <fly1ngDream@protonamil.com>
2019-08-23 16:27:30 +0300

    Change elixir version to 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule StreamLiveBot.MixProject do
     [
       app: :stream_live_bot,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
